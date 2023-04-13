66a66206db9637b3ee95bb07265f26cefa71bf82
Red <>
2023-04-13 10:35:48 +0530

    Update Date

--- lib/Utils/api_http.dart
+++ lib/Utils/api_http.dart
@@ -8,7 +8,7 @@ class NewsData
   Future<NewsModel?> getNewsData(country,cat)async
   {
     var response = await http.get(
-        Uri.parse("https://newsapi.org/v2/everything?q=$country($cat)&from=${DateTime.now().year}-${DateTime.now().month}-${DateTime.now().day}&to=${DateTime.now().year}-${DateTime.now().month}-${DateTime.now().day}&sortBy=popularity&apiKey=dd639a0a034f4c77b0fde97fb5ea581d")
+        Uri.parse("https://newsapi.org/v2/everything?q=$country($cat)&from=${DateTime.now().year}-${DateTime.now().month}-${DateTime.now().day - 1}&to=${DateTime.now().year}-${DateTime.now().month}-${DateTime.now().day}&sortBy=popularity&apiKey=dd639a0a034f4c77b0fde97fb5ea581d")
     );
     if(response.statusCode==200)
     {
